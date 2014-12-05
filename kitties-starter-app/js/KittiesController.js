112ac86e687b1493efb46a3aec2b8b91a557cb78
fsp712 <fainashalts@gmail.com>
2014-12-05 15:37:43 -0800

    first draft of cat adoption app lab

---
+++ kitties-starter-app/js/KittiesController.js
@@ -0,0 +1,39 @@
+angular
+	.module('adoptionApp')
+	.controller('AdoptionController', AdoptionController);
+
+function AdoptionController(){
+
+	this.catList = [
+		{
+			catName: 'Karl',
+			monthsOld: 5,
+			gender: 'boy',
+			image: "images/carl.png",
+			adopted: true,
+		},
+		{
+			catName: 'Jack',
+			monthsOld: 4,
+			gender: 'boy',
+			image: "images/jack.png",
+			adopted: false
+		},
+		{
+			catName: 'Oscar',
+			monthsOld: 2,
+			gender: 'boy',
+			image: "images/oscar.png",
+			adopted: false
+		},
+		{
+			catName: 'Princess Mew',
+			monthsOld: 3,
+			gender: 'girl',
+			image: "images/princessmew.png",
+			adopted: false
+		}
+
+	];
+
+}
